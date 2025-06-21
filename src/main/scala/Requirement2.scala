00f936321ee583b4f95cda58648cf5856d90d158
Govindmall <govind.mall0704@gmail.com>
2025-06-21 17:25:00 +0530

    Display the number of units sold in each region

--- src/main/scala/Requirement2.scala
+++ src/main/scala/Requirement2.scala
@@ -1,16 +1,16 @@
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
-
 object Requirement2{
   def main(args: Array[String]): Unit = {
     // created Spark Session
     val spark = SparkSession.builder().appName("DistinctCountries").master("local[*]").getOrCreate()
     //read the csv file into DataFrame
     val salesDF = spark.read.option("header", "true").csv("C:/Users/gomall/Downloads/sparsales.csv")
-    //get distinct countries
-
-    //saving as a parquet file
-    //countriesDF.write.parquet("C:/Users/gomall/Desktop/result1.parquet")
+    //compute total units sold per region
+    val regionSalesDF=salesDF.groupBy("Region").agg(sum("Units Sold").as("Total_unit_sold"))
+    //show the regionSalesDF in console
+    regionSalesDF.show()
+    regionSalesDF.write.parquet("C:/Users/gomall/Desktop/2.parquet")
     spark.stop()
 
 
